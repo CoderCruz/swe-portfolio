6964400c245cc64da4a09c65009da4711490b270
CoderCruz <cruzjosework@gmail.com>
2025-06-03 15:51:10 -0400

    added email, linkedin and github icons to nav bar. elements are responsive for mobile, tablet and web

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,14 +1,41 @@
 import Link from 'next/link';
+import { FaEnvelope, FaGithub, FaLinkedin } from 'react-icons/fa'
 
 export default function Nav() {
   return(
-    <nav className="w-full px-4 py-3 md:px-8 md:py-4 bg-transparent">
+    <nav className="w-full flex justify-between px-4 py-3 md:px-8 md:py-4 bg-transparent">
       <Link
         href="/"
         className="text-2xl md:text-3xl font-extrabold tracking-wide bg-clip-text text-transparent bg-gradient-to-r from-gray-500 to-white hover:from-gray-900 hover:to-white transition-all duration-500"
       >
         CRUZCODE
       </Link>
+
+      <div className="flex gap-2 items-center text-xl md:text-3xl ">
+        <Link 
+          href="mailto:cruzjosework@gmail.com"
+          rel="noopener noreferrer"
+          className="text-white hover:text-gray-800 transition-colors duration-300"
+        > 
+          <FaEnvelope />
+        </Link>
+        <Link 
+          href="https://github.com/CoderCruz" 
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-white hover:text-gray-800 transition-colors duration-300"
+        >
+          <FaGithub />
+        </Link>
+        <Link 
+          href="https://www.linkedin.com/in/josecruzsoftware/" 
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-white hover:text-gray-800 transition-colors duration-300"
+        >
+          <FaLinkedin />
+        </Link>
+      </div>
     </nav>
   )
 }
